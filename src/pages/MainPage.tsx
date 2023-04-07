001a15f6ecec8f42f042e7924d4d8d06b38e3071
dannysmson <dannysmson@naver.com>
2023-04-07 19:29:28 +0900

    feat : 카드아이템 컴포 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,16 +7,21 @@ import StyledCard from '../components/CardItem';
 const MainPageContainer = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
-  gap: 4rem;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 120rem;
+  gap: 4rem;
+`;
 const TagContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 0.8rem;
-  width: 120rem;
+  gap: 1.6rem;
+  width: 100%;
 `;
 
 const TagListContainer = styled.div`
@@ -32,7 +37,16 @@ const ListContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 2rem;
-  width: 120rem;
+  width: 100%;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  column-gap: 4rem;
+  row-gap: 6rem;
+  padding: 0.4rem 2.4rem 2rem;
 `;
 
 const icons = require.context('../assets/icons', true);
@@ -40,45 +54,119 @@ const icons = require.context('../assets/icons', true);
 function MainPage() {
   return (
     <MainPageContainer className="MainPage">
-      <TagContainer>
-        <span className={Styles.h3}>종류</span>
-        <TagListContainer>
-          <StyledTag imgSrc={icons('./korea.png')} text="한식" />
-          <StyledTag imgSrc={icons('./china.png')} text="중식" />
-          <StyledTag imgSrc={icons('./japan.png')} text="일식" />
-          <StyledTag imgSrc={icons('./us.png')} text="양식" />
-        </TagListContainer>
-        <span className={Styles.h3}>태그</span>
-        <TagListContainer>
-          <StyledTag imgSrc={icons('./korea.png')} text="한식" />
-          <StyledTag imgSrc={icons('./china.png')} text="중식" />
-          <StyledTag imgSrc={icons('./japan.png')} text="일식" />
-          <StyledTag imgSrc={icons('./us.png')} text="양식" />
-        </TagListContainer>
-        <span className={Styles.h3}>함께먹기</span>
-        <TagListContainer>
-          <StyledTag imgSrc={icons('./korea.png')} text="한식" />
-          <StyledTag text="+" />
-          <StyledTag text="제로페이" />
-        </TagListContainer>
-      </TagContainer>
-      <ListContainer>
-        <StyledCard
-          imgSrc=""
-          likes="12개"
-          name="마리모"
-          category="돈까스, 우동"
-          hashtag="#일식 가정식 #혼밥 #제로페이"
-        />
-        <StyledCard
-          imgSrc=""
-          likes="12개"
-          name="마리모"
-          category="돈까스, 우동"
-          hashtag="#일식 가정식 #혼밥 #제로페이"
-          showIconBox={false}
-        />
-      </ListContainer>
+      <Container>
+        <TagContainer>
+          <span className={Styles.h3}>종류</span>
+          <TagListContainer>
+            <StyledTag imgSrc={icons('./korea.png')} text="한식" />
+            <StyledTag imgSrc={icons('./china.png')} text="중식" />
+            <StyledTag imgSrc={icons('./japan.png')} text="일식" />
+            <StyledTag imgSrc={icons('./us.png')} text="양식" />
+          </TagListContainer>
+          <span className={Styles.h3}>태그</span>
+          <TagListContainer>
+            <StyledTag imgSrc={icons('./korea.png')} text="한식" />
+            <StyledTag imgSrc={icons('./china.png')} text="중식" />
+            <StyledTag imgSrc={icons('./japan.png')} text="일식" />
+            <StyledTag imgSrc={icons('./us.png')} text="양식" />
+          </TagListContainer>
+          <span className={Styles.h3}>함께먹기</span>
+          <TagListContainer>
+            <StyledTag imgSrc={icons('./korea.png')} text="한식" />
+            <StyledTag text="+" />
+            <StyledTag text="제로페이" />
+          </TagListContainer>
+        </TagContainer>
+        <ListContainer>
+          <span className={Styles.h3}>진정한 한국인의 추천 리스트</span>
+          <GridContainer>
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+            <StyledCard
+              imgSrc=""
+              likes="12개"
+              name="마리모"
+              category="돈까스, 우동"
+              hashtag="#일식 가정식 #혼밥 #제로페이"
+            />
+          </GridContainer>
+        </ListContainer>
+      </Container>
     </MainPageContainer>
   );
 }
