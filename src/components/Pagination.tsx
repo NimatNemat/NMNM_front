10ab39bd4bb907c42fb5341d5b187782a7691cbc
dannysmson <dannysmson@naver.com>
2023-04-21 09:44:54 +0900

    style : 페이지네이션 UI 변경

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 interface pageProps {
@@ -6,61 +6,97 @@ interface pageProps {
   totalPosts: number;
   paginate: (data: number) => void;
 }
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
 const PageUl = styled.ul`
-  float: left;
-  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  border-radius: 3px;
-  color: white;
-  padding: 1px;
-  border-top: 3px solid #186ead;
-  border-bottom: 3px solid #186ead;
-  background-color: rgba(0, 0, 0, 0.4);
+  padding: 0;
 `;
 
-const PageLi = styled.li`
-  display: inline-block;
-  font-size: 17px;
-  font-weight: 600;
-  padding: 5px;
-  border-radius: 5px;
-  width: 25px;
+const PageSpan = styled.button<{ active?: boolean }>`
+  border: ${({ active }) => (active ? '2px solid #000' : 'none')};
+  cursor: pointer;
+  height: 4rem;
+  width: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({ active }) => (active ? '#FFFFFF' : '#FFFDF5')};
+
   &:hover {
-    cursor: pointer;
-    color: white;
-    background-color: #263a6c;
-  }
-  &:focus::after {
-    color: white;
-    background-color: #263a6c;
-  }
-`;
-
-const PageSpan = styled.button`
-  &:hover::after,
-  &:focus::after {
-    border-radius: 100%;
-    color: white;
-    background-color: #263a6c;
+    border: 2px solid #000;
   }
 `;
 
 function Pagination(props: pageProps) {
   const { postsPerPage, totalPosts, paginate } = props;
+  const [currentPageSet, setCurrentPageSet] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPageButtons = 10;
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i += 1) {
+  for (let i = 1; i <= totalPages; i += 1) {
     pageNumbers.push(i);
   }
+
+  const indexOfLastButton = currentPageSet * maxPageButtons;
+  const indexOfFirstButton = indexOfLastButton - maxPageButtons;
+  const currentButtons = pageNumbers.slice(indexOfFirstButton, indexOfLastButton);
+
+  const nextPage = () => {
+    if (currentPageSet < Math.ceil(totalPages / maxPageButtons)) {
+      setCurrentPageSet(currentPageSet + 1);
+      paginate(indexOfLastButton + 1);
+      setCurrentPage(indexOfLastButton + 1);
+    }
+  };
+  const prevPage = () => {
+    if (currentPageSet > 1) {
+      setCurrentPageSet(currentPageSet - 1);
+      paginate(indexOfFirstButton);
+      setCurrentPage(indexOfFirstButton);
+    }
+  };
+  const firstPage = () => {
+    setCurrentPageSet(1);
+    paginate(1);
+    setCurrentPage(1);
+  };
+  const LastPage = () => {
+    setCurrentPageSet(Math.ceil(totalPages / maxPageButtons));
+    paginate(totalPages);
+    setCurrentPage(totalPages);
+  };
+
   return (
-    <div>
-      <nav>
-        <PageUl>
-          {pageNumbers.map((number) => (
-            <PageSpan onClick={() => paginate(number)}>{number}</PageSpan>
-          ))}
-        </PageUl>
-      </nav>
-    </div>
+    <StyledNav>
+      <PageUl>
+        <PageSpan onClick={firstPage}>&lt;&lt;</PageSpan>
+        <PageSpan onClick={prevPage}>&lt;</PageSpan>
+        {currentButtons.map((number) => (
+          <PageSpan
+            key={number}
+            active={number === currentPage}
+            onClick={() => {
+              paginate(number);
+              setCurrentPage(number);
+            }}
+          >
+            {number}
+          </PageSpan>
+        ))}
+        <PageSpan onClick={nextPage}>&gt;</PageSpan>
+        <PageSpan onClick={LastPage}>&gt;&gt;</PageSpan>
+      </PageUl>
+    </StyledNav>
   );
 }
 
